5adedbfaefd5193b87a5f7176c06b2b0d2e37301
Yuliia Sukhova <yuliia.sukhova@tech-stack.io>
2021-02-16 11:36:51 +0200

    lesson2-updates

--- src/test/java/lesson2/RegistrationFormTest.java
+++ src/test/java/lesson2/RegistrationFormTest.java
@@ -1,15 +1,17 @@
 package lesson2;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -32,7 +34,9 @@ public class RegistrationFormTest {
         String emailAddress = String.format("johndou%s@yopmail.com", RandomStringUtils.randomNumeric(4));
         String gender = "Male";
         String mobile = RandomStringUtils.randomNumeric(10);
-        String dateOfBirth = "16 May,2003";
+        String dateOfBirth = "16";
+        String monthOfBirth = "May";
+        String yearOfBirth = "2003";
         String subject = "Arts";
         String hobby = "Reading";
         String picture = "test-img.png";
@@ -45,7 +49,7 @@ public class RegistrationFormTest {
         expectedDataList.add(emailAddress);
         expectedDataList.add(gender);
         expectedDataList.add(mobile);
-        expectedDataList.add(dateOfBirth);
+        expectedDataList.add(String.format("%s %s,%s", dateOfBirth, monthOfBirth, yearOfBirth));
         expectedDataList.add(subject);
         expectedDataList.add(hobby);
         expectedDataList.add(picture);
@@ -56,36 +60,29 @@ public class RegistrationFormTest {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(emailAddress);
-        $(By.xpath(String.format(".//label[text()='%s']", gender))).click();
+        $(byText(gender)).click();
         $("#userNumber").setValue(mobile);
         $("#dateOfBirthInput").click();
-        $(By.xpath(".//select[contains(@class,'datepicker__month')]")).selectOptionByValue("4");
-        $(By.xpath(".//div[contains(@class,'datepicker__year')]")).click();
-        $(By.xpath(".//select[contains(@class,'datepicker__year')]")).selectOptionByValue("2003");
-        $(By.xpath(".//div[contains(@class,'month-dropdown')]")).click();
-        $(By.xpath(".//div[contains(@class,'react-datepicker__week')]/div[contains(text(),'16')]")).click();
-        $(By.xpath(".//div[@id='subjectsContainer']")).click();
-        $(By.xpath(".//input[@id='subjectsInput']")).setValue(subject)
-                                                    .pressEnter();
-        $("#hobbies-checkbox-2").parent()
-                                .click();
+        $(".react-datepicker__month-select").selectOptionByValue("4");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOptionByValue("2003");
+        $x(".//div[contains(@class,'react-datepicker__week')]/div[contains(text(),'16')]").click();
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbies-checkbox-2").parent().click();
         $("#currentAddress").setValue(address);
-        $(By.xpath(".//div[text()='Select State']")).scrollIntoView(true)
-                                                    .click();
-        $(By.xpath(".//div[contains(@id,'1')]")).click();
-        $(By.xpath(".//div[text()='Select City']")).click();
-        $(By.xpath(".//div[contains(@id,'2')]")).click();
-
-        File cv = new File(String.format("src/test/resources/%s", picture));
-        $("#uploadPicture").uploadFile(cv);
+        $(byText("Select State")).scrollIntoView(true).click();
+        $x(".//div[contains(@id,'1')]").click();
+        $(byText("Select City")).click();
+        $x(".//div[contains(@id,'2')]").click();
+        $("#uploadPicture").uploadFile(new File(String.format("src/test/resources/%s", picture)));
         $("#submit").click();
 
         ElementsCollection userDataElementsList = $$(By.xpath(".//tr/td[2]"));
         List<String> userDataInModal = userDataElementsList.texts();
 
         $(By.className("modal-open")).shouldBe(visible);
-
-        Assertions.assertLinesMatch(expectedDataList, userDataInModal, "Incorrect userdata was displayed in " +
+        assertEquals(expectedDataList, userDataInModal, "Incorrect user data was displayed in " +
                 "registration modal");
 
         $("#closeLargeModal").click();
